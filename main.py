160d18d4939d4aaf53f82a9c6e37b31b318e500b
kms <kmslkh@naver.com>
2021-10-07 15:19:28 +0900

    [ADD] selenium TEST

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver"
+LOA_AUCTION_URL = "https://lostark.game.onstove.com/Auction"
+
+
 if __name__ == '__main__':
-    print("INIT")
+    driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
+    driver.get(LOA_AUCTION_URL)
+    id_el = driver.find_element_by_id("user_id")
+    id = input("id : ")
+    id_el.send_keys(id)
+    pw_el = driver.find_element_by_id("user_pwd")
+    pw = input("pw : ")
+    pw_el.send_keys(pw)
+    pw_el.send_keys(Keys.ENTER)
